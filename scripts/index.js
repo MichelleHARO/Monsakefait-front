493b3c018ed1b790f3bc4b13c1af9505b6e2035d
hugoperhirin <hugodoggydogg@gmail.com>
2024-05-17 14:46:11 +0200

    :build: Modified homepage.html, added a basic JS script for it and also a simple CSS + basic JS script for index.html

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // code de connexion ici
+});
+
+document.getElementById('signup-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // code d'inscription ici
+});
