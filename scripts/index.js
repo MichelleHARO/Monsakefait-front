20c36b67a41eb11fcc5a2ae38d04d557e1aac40d
hugoperhirin <hugodoggydogg@gmail.com>
2024-05-19 15:55:51 +0200

    :build: Starting from scratch w/ React

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('login-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    // code de connexion ici
-});
-
-document.getElementById('signup-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    // code d'inscription ici
-});
