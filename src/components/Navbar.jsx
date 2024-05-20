2e5dd826a2e037d1945f89eb8ab776092908f44e
hugoperhirin <hugodoggydogg@gmail.com>
2024-05-20 15:08:47 +0200

    :build::fix: Made a DrawerContent file for future content like cards / Fixed navbar with a drawer instead of a simple burger menu dropdown display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,53 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 const Navbar = () => {
-    const [showMonsak, setShowMonsak] = useState(false);
-    const userEmail = "prout@prout.prout"
+    const userEmail = "prout@prout.prout";
 
-    const toggleMonsak = () => {
-        setShowMonsak(!showMonsak);
-    };
-
-    return (
+    return (<div>
         <div className="navbar bg-primary text-primary-content rounded-lg">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">Monsakefait</a>
             </div>
             <div className="flex-none">
-                <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle" onClick={toggleMonsak}>
+                <label htmlFor="my-drawer-4" className="btn btn-ghost btn-circle drawer-button">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
                              stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                   d="M4 6h16M4 12h16m-7 6h7"/>
                         </svg>
-                    </div>
-                    {showMonsak && (
-                        <ul tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="flex items-center">
-                                    <img
-                                        alt="User avatar"
-                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                                        className="w-6 h-6 rounded-full mr-2"
-                                    />
-                                    <span>Monsak</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a>
-                                    <span className="text-sm">{userEmail}</span>
-                                </a>
-                            </li>
-                            <li><a>Se déconnecter</a></li>
-                        </ul>
-                    )}
-                </div>
+                </label>
             </div>
         </div>
-    );
+
+        <div className="drawer z-20 drawer-end">
+            <input id="my-drawer-4" type="checkbox" className="drawer-toggle"/>
+            <div className="drawer-content">
+                {/* Page content here */}
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-1/2 min-h-full bg-base-200 text-base-content">
+                    {/* Sidebar content here */}
+                    <li className="mb-4">
+                        <a className="flex items-center">
+                            <img
+                                alt="User avatar"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                                className="w-6 h-6 rounded-full mr-2"
+                            />
+                            <span>Monsak</span>
+                        </a>
+                    </li>
+                    <li className="mb-4">
+                        <a>
+                            <span className="text-sm">{userEmail}</span>
+                        </a>
+                    </li>
+                    <li><a>Se déconnecter</a></li>
+                </ul>
+                </div>
+            </div>
+    </div>);
 }
 
 export default Navbar;
