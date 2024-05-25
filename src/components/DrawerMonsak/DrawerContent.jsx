2140eddc25932eb24d21a2b47e7032f271fc5ce4
hugoperhirin <hugodoggydogg@gmail.com>
2024-05-25 13:22:38 +0200

    :feat: handledelete function

--- src/components/DrawerMonsak/DrawerContent.jsx
+++ src/components/DrawerMonsak/DrawerContent.jsx
@@ -1,15 +1,18 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import MonsakButton from './MonsakButton.jsx';
 import MonsakAccordion from "./MonsakAccordion.jsx";
 
 const DrawerContent = () => {
-    const MonsakArray = [
-        { id: 1, desc: "Un prout nucléaire", items: [{ name: "Une vulve" }, { name: "Un zizi" }, { name: "Une vache" }, { name: "Un sexe" }] },
-        { id: 2, desc: "Une femme avec zizi", items: [] },
-        { id: 3, desc: "Une vulvo vache", items: [] },
-        { id: 4, desc: "Un sexe velu", items: [] },
-    ];
+    const MonsakArray = [{
+        id: 1,
+        desc: "Un prout nucléaire",
+        items: [{name: "Une vulve"}, {name: "Un zizi"}, {name: "Une vache"}, {name: "Un sexe"}]
+    }, {id: 2, desc: "Une femme avec zizi", items: []}, {id: 3, desc: "Une vulvo vache", items: []}, {
+        id: 4,
+        desc: "Un sexe velu",
+        items: []
+    },];
 
     const [openBagAccordion, setOpenBagAccordion] = useState(1);
     const [messak, setMessak] = useState(MonsakArray);
@@ -24,27 +27,21 @@ const DrawerContent = () => {
     console.log("Current Messak Array:", messak);
     console.log("Open Bag Accordion ID:", openBagAccordion);
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-full p-4">
+    return (<div className="flex flex-col items-center justify-center min-h-full p-4">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {messak.map((monsak) => (
-                    <MonsakButton
+                {messak.map((monsak) => (<MonsakButton
                         key={monsak.id}
                         id={monsak.id}
                         setOpenBagAccordion={setOpenBagAccordion}
                         handleDelete={handleBagDelete}
-                    />
-                ))}
+                    />))}
             </div>
-            {messak.map((monsak) => (
-                <MonsakAccordion
+            {messak.map((monsak) => (<MonsakAccordion
                     key={monsak.id}
                     monsak={monsak}
                     openBagAccordion={openBagAccordion}
-                />
-            ))}
-        </div>
-    );
+                />))}
+        </div>);
 };
 
 export default DrawerContent;
