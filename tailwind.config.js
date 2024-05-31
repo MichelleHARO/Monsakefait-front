08fab22320b33957f1917c582a03fe62c2b65a71
hugoperhirin <hugodoggydogg@gmail.com>
2024-05-31 13:33:19 +0200

    :design: modifed TW cfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightOrange: '#FF9F1C',
+        lightOrange: '#FFBF69',
+        mutedPink: '#CB997E',
+        palePeach: '#FFE8D6',
+      },
+    },
   },
   plugins: [
-    require('daisyui')
+    require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          'primary': '#FF9F1C',
+          'secondary': '#FFBF69',
+          'accent': '#CB997E',
+          'neutral': '#3D4451',
+          'base-100': '#FFFFFF',
+          'info': '#2094F3',
+          'success': '#009485',
+          'warning': '#FF9900',
+          'error': '#FF5724',
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 }
