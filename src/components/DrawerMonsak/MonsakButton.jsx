7b28f48e83a8907edbf998bedde43b0f5c2d59b5
hugoperhirin <hugodoggydogg@gmail.com>
2024-06-03 00:22:00 +0200

    :design: darkmode toggle working

--- src/components/DrawerMonsak/MonsakButton.jsx
+++ src/components/DrawerMonsak/MonsakButton.jsx
@@ -1,9 +1,8 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import axiosInstance from '../StandAlone/axiosInstance';
 
-// eslint-disable-next-line react/prop-types
-const MonsakButton = ({id, setOpenBagAccordion}) => {
+const MonsakButton = ({ id, setOpenBagAccordion, theme }) => {
+
     const handleClick = (id) => {
         setOpenBagAccordion(id);
     };
@@ -13,11 +12,11 @@ const MonsakButton = ({id, setOpenBagAccordion}) => {
 
         try {
             const responseDelete = await axiosInstance.delete(`http://localhost:3001/api/me/bag/${id}`);
-            console.log("Server response :", responseDelete.data)
+            console.log("Server response :", responseDelete.data);
             const newToken = responseDelete.data.newToken;
             if (newToken) {
                 localStorage.setItem('token', newToken);
-                console.log('Token updated in localStorage')
+                console.log('Token updated in localStorage');
             }
         } catch (error) {
             if (error.response && error.response.status === 401) {
@@ -27,17 +26,18 @@ const MonsakButton = ({id, setOpenBagAccordion}) => {
                 console.error("Erreur while deleting from Monsak !", error);
             }
         }
-    }
+    };
 
-    return (<div>
+    return (
+        <div>
             <button
                 type="button"
                 className="btn btn-active btn-secondary flex items-center justify-center shadow-xl mb-4"
                 onClick={() => handleClick(id)}
             >
-    <span className="font-display font-light text-center">
-      Monsak  <span className="text-base-200 font-display text-outline text-shadow-lg">{id}</span>
-    </span>
+                <span className="font-display font-light text-center">
+                    Monsak <span className={`text-primary font-display text-outline text-shadow-lg ${theme === 'dark' ? 'text-white' : 'text-black'}`}>{id}</span>
+                </span>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -45,17 +45,17 @@ const MonsakButton = ({id, setOpenBagAccordion}) => {
                     strokeWidth={1.5}
                     stroke="currentColor"
                     className="h-5 w-5 text-red-500 ml-2"
-                    style={{stroke: 'black', strokeWidth: '2px'}}
+                    style={{ stroke: 'black', strokeWidth: '2px' }}
                     onClick={(e) => {
                         e.stopPropagation();
                         handleBagDelete(id);
                     }}
                 >
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </button>
         </div>
     );
-}
+};
 
 export default MonsakButton;
--- src/components/HomePage/Navbar.jsx
+++ src/components/HomePage/Navbar.jsx
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axiosInstance from '../StandAlone/axiosInstance.jsx';
 import DrawerContent from '../DrawerMonsak/DrawerContent.jsx';
+import DarkModeHandler from '../StandAlone/darkModeHandler.jsx';
+import logoLight from '../../assets/Logov2.svg';
+import logoDark from '../../assets/logonavdark.svg';
+
 
 const Navbar = () => {
     const [userEmail, setUserEmail] = useState('prout@prout.prout');
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+    );
 
     useEffect(() => {
         const fetchUserInfo = async () => {
@@ -19,19 +26,24 @@ const Navbar = () => {
         fetchUserInfo();
     }, []);
 
+    const handleThemeChange = (newTheme) => {
+        setTheme(newTheme);
+    };
+
     return (
         <div className="navbar-fixed navbar-container mx-4">
             <div className="navbar bg-base-100 text-primary-content rounded-b-lg h-20 shadow-lg flex justify-between items-center">
                 <div className="flex items-center">
                     <img
-                        src='../src/assets/Logov2.svg'
+                        src={theme === 'light' ? logoLight : logoDark}
                         alt="Logo"
                         className="h-16 pl-6"
                     />
                 </div>
                 <div className="flex items-center space-x-4">
+                    <DarkModeHandler onThemeChange={handleThemeChange} />
                     <label htmlFor="my-drawer-4" className="btn btn-ghost btn-circle drawer-button">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                         </svg>
                     </label>
@@ -46,7 +58,7 @@ const Navbar = () => {
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-3/4 min-h-full bg-base-200 rounded-lg shadow-inner">
-                        <li className="mb-4 flex items-center justify-between">
+                        <li className="mb-4 flex items-center">
                             <label htmlFor="my-drawer-4" className="btn btn-ghost btn-circle drawer-button ml-auto mr-4">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="h-6 w-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
