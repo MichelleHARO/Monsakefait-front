cb2f4ce11118c8aadd773290a22bbdbdd0e9ed67
hugoperhirin <hugodoggydogg@gmail.com>
2024-06-02 19:07:47 +0200

    :design: more darkmode bs

--- src/hooks/useDark.jsx
+++ src/hooks/useDark.jsx
@@ -1,7 +1,20 @@
+// src/hooks/useDark.jsx
 import { useState, useEffect } from "react";
 
 export default function useDarkSide() {
-    const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "light");
+    const getInitialTheme = () => {
+        // Check local storage first
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme) {
+            return savedTheme;
+        }
+
+        // If not found in local storage, use system preference
+        const userPrefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches;
+        return userPrefersDark ? "dark" : "light";
+    };
+
+    const [theme, setTheme] = useState(getInitialTheme);
 
     useEffect(() => {
         const root = window.document.documentElement;
