98837d0706b1cad94f8e3d193a22a90514c301db
hugoperhirin <hugodoggydogg@gmail.com>
2024-06-04 14:47:39 +0200

    :fix: drawer UI fix

--- src/components/DrawerMonsak/MonsakAccordion.jsx
+++ src/components/DrawerMonsak/MonsakAccordion.jsx
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
 import BagItem from "../StandAlone/BagItem.jsx";
 import axiosInstance from "../StandAlone/axiosInstance.jsx";
-import { useApiUrl} from "../../context/ApiUrlContext.jsx";
+import { useApiUrl } from "../../context/ApiUrlContext.jsx";
 
-
-// eslint-disable-next-line react/prop-types
 const MonsakAccordion = ({ monsak, openBagAccordion }) => {
-    // eslint-disable-next-line react/prop-types
     const { id, name, image, description, items } = monsak;
 
-    // Initialiser à true pour que le bouton soit transparent par défaut
     const [isButtonClicked, setIsButtonClicked] = useState(true);
     const [selectedItem, setSelectedItem] = useState(null);
     const [allItems, setAllItems] = useState([]);
@@ -19,14 +15,14 @@ const MonsakAccordion = ({ monsak, openBagAccordion }) => {
     useEffect(() => {
         const fetchItems = async () => {
             try {
-                const response = await axiosInstance.get(`${apiUrl}/api/me/item`)
-                setAllItems(response.data)
-            }catch(error){
-                console.error("Error fetching collection", error)
+                const response = await axiosInstance.get(`${apiUrl}/api/me/item`);
+                setAllItems(response.data);
+            } catch (error) {
+                console.error("Error fetching collection", error);
             }
         };
         fetchItems();
-    }, [])
+    }, []);
 
     const handleChangeSelect = (event) => {
         setSelectedItem(event.target.value);
@@ -36,22 +32,18 @@ const MonsakAccordion = ({ monsak, openBagAccordion }) => {
         setIsButtonClicked(!isButtonClicked);
     };
 
-    //handle 
     const handleAddClick = async () => {
-        //console.log(selectedItem, id)
         try {
-            const response = await axiosInstance.post(`${apiUrl}/api/me/item/addItem/${id}`,
-            { selectedItem });
-            //console.log("Server response :", response.data)
+            const response = await axiosInstance.post(`${apiUrl}/api/me/item/addItem/${id}`, { selectedItem });
             const newToken = response.data.newToken;
             if (newToken) {
                 localStorage.setItem('token', newToken);
-                console.log('Token updated in localStorage')
+                console.log('Token updated in localStorage');
             }
         } catch (error) {
-            console.error("Error while adding Item in Monsak !", error)
+            console.error("Error while adding Item in Monsak!", error);
         }
-    }
+    };
 
     return (
         <div className="collapse collapse-arrow bg-base-200">
@@ -66,35 +58,35 @@ const MonsakAccordion = ({ monsak, openBagAccordion }) => {
             </div>
             <div className="collapse-content">
                 <p className="font-bold">{description}</p>
-                    <div className="mt-4">
-                        {items &&
-                            items.map((item, index) => (
-                                <BagItem key={index} item={item} />
-                            ))}
-                    </div>
-                <div className="flex justify-center m-4 join join-vertical lg:join-horizontal">
-                    <select 
-                        value={selectedItem} 
+                <div className="mt-4">
+                    {items && items.map((item, index) => (
+                        <BagItem key={index} item={item} />
+                    ))}
+                </div>
+                <div className="flex flex-col items-center lg:flex-row lg:justify-center lg:space-x-4 space-y-4 lg:space-y-0 m-4">
+                    <select
+                        value={selectedItem}
                         onChange={handleChangeSelect}
-                        className="select select-bordered  w-full select-sm max-w-xs join-item">
+                        className="select select-bordered w-full max-w-xs"
+                    >
                         <option disabled selected>Ajouter un objet à Monsak {id}</option>
                         {allItems && allItems.map((item) => (
                             <option key={item.id} value={item.id}>{item.name}</option>
-                        ))}                        
+                        ))}
                     </select>
                     <button
                         onClick={handleAddClick}
-                        className="btn btn-sm font-display mt-4 bg-transparent border-gray-100 shadow-xl max-w-xs join-item">
+                        className="btn btn-sm font-display bg-transparent border-gray-100 shadow-xl"
+                    >
                         Ajouter objet !
                     </button>
                 </div>
                 <div className="flex justify-center mb-3">
                     <button
                         onClick={handleButtonClick}
-                        className={`btn font-display mt-4 ${
-                            isButtonClicked
-                                ? 'bg-transparent border-gray-100 shadow-xl'
-                                : 'bg-green-500 border-green-500 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 mb-3'
+                        className={`btn font-display mt-4 ${isButtonClicked
+                            ? 'bg-transparent border-gray-100 shadow-xl'
+                            : 'bg-green-500 border-green-500 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 mb-3'
                         }`}
                     >
                         Monsakefait !
--- src/components/StandAlone/BagItem.jsx
+++ src/components/StandAlone/BagItem.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import axiosInstance from "./axiosInstance";
-import { useApiUrl} from "../../context/ApiUrlContext.jsx";
+import { useApiUrl } from "../../context/ApiUrlContext.jsx";
 
 // eslint-disable-next-line react/prop-types
 const BagItem = ({ item }) => {
-    // eslint-disable-next-line react/prop-types
     const { id, name, bag_contains_item } = item;
-    const [isChecked, setIsChecked] = useState(false); // Initialisez avec false pour ne pas être coché par défaut
+    const [isChecked, setIsChecked] = useState(false);
 
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
@@ -15,118 +14,72 @@ const BagItem = ({ item }) => {
     const apiUrl = useApiUrl();
 
     const handleAddQuantity = async () => {
-        //console.log("click", id, bag_contains_item );
         try {
             const response = await axiosInstance.post(`${apiUrl}/api/me/item/addQuantity/${id}`, { bag_contains_item });
-            //console.log("Server response :", response.data)
             const newToken = response.data.newToken;
             if (newToken) {
                 localStorage.setItem('token', newToken);
-                console.log('Token updated in localStorage')
+                console.log('Token updated in localStorage');
             }
         } catch (error) {
-            console.error("Erreur while updating item quantity !", error)
+            console.error("Erreur while updating item quantity!", error);
         }
     };
 
     const handleRemoveQuantity = async () => {
-        //console.log("click", id, bag_contains_item );
         try {
-        const response = await axiosInstance.delete(`${apiUrl}/api/me/item/removeQuantity/${id}`, { params: { bag_contains_item }});
-        //console.log("Server response :", response.data)
-        const newToken = response.data.newToken;
-        if (newToken) {
-            localStorage.setItem('token', newToken);
-            console.log('Token updated in localStorage')
-        }
-    } catch (error) {
-        console.error("Erreur while updating item quantity !", error)
-    }
-    };
-
-    const handleDeleteItem = async () => {
-        //console.log("click", id, bag_contains_item );
-        try {
-            const response = await axiosInstance.delete(`${apiUrl}/api/me/item/removeItem/${id}`,
-            // ajout de { params: {} } pour envoyer bag_contains_item dans la requête .delete
-            { params: { bag_contains_item }});
-            //console.log("Server response :", response.data)
+            const response = await axiosInstance.delete(`${apiUrl}/api/me/item/removeQuantity/${id}`, { params: { bag_contains_item } });
             const newToken = response.data.newToken;
             if (newToken) {
                 localStorage.setItem('token', newToken);
-                console.log('Token updated in localStorage')
+                console.log('Token updated in localStorage');
             }
         } catch (error) {
-            console.error("Erreur while removing Item from Monsak !", error)
+            console.error("Erreur while updating item quantity!", error);
         }
     };
 
+    const handleDeleteItem = async () => {
+        try {
+            const response = await axiosInstance.delete(`${apiUrl}/api/me/item/removeItem/${id}`, { params: { bag_contains_item } });
+            const newToken = response.data.newToken;
+            if (newToken) {
+                localStorage.setItem('token', newToken);
+                console.log('Token updated in localStorage');
+            }
+        } catch (error) {
+            console.error("Erreur while removing Item from Monsak!", error);
+        }
+    };
 
     return (
-        <div className="flex items-center w-full">
-            <input
-                type="checkbox"
-                className="checkbox mr-2"
-                checked={isChecked}
-                onChange={handleCheckboxChange}
-            />
-            <div
-                //onClick={handleDelete}
-                className="join flex justify-between items-center w-full"
-            >
-                
-                <span 
-                    className="font-bold join-item">{name}</span>
-                <div>
-                    <span 
-                        className="font-bold text-center p-2 join-item">
-                        Qté: {bag_contains_item.quantity}
-                    </span>
-                    <button
-                        className="btn btn-sm">
-                        <svg 
-                            onClick={handleRemoveQuantity}
-                            xmlns="http://www.w3.org/2000/svg" 
-                            viewBox="0 0 24 24" fill="currentColor" 
-                            className="size-6 text-red-500 join-item"
-                        >
-                            <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
-                        </svg>
-                    </button>
-                    
-                    <button
-                        className="btn btn-sm">
-                        <svg 
-                            onClick={handleAddQuantity} 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            viewBox="0 0 24 24" 
-                            fill="currentColor" 
-                            className="size-6 text-green-500 join-item">
-                            <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
-                        </svg>
-                    </button>
-                    
-
-                    <button
-                        className="btn btn-sm">
-                        <svg
-                            onClick={handleDeleteItem}
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6 text-red-500 join-item"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
-                    </button>
-                    
-                </div>
+        <div className="flex flex-col md:flex-row items-center justify-between w-full p-2">
+            <div className="flex items-center">
+                <input
+                    type="checkbox"
+                    className="checkbox mr-2"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                />
+                <span className="font-bold">{name}</span>
+            </div>
+            <div className="flex items-center space-x-2 mt-2 md:mt-0">
+                <span className="font-bold text-center">Qté: {bag_contains_item.quantity}</span>
+                <button className="btn btn-sm" onClick={handleRemoveQuantity}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 text-red-500">
+                        <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+                    </svg>
+                </button>
+                <button className="btn btn-sm" onClick={handleAddQuantity}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 text-green-500">
+                        <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+                    </svg>
+                </button>
+                <button className="btn btn-sm" onClick={handleDeleteItem}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
             </div>
         </div>
     );
